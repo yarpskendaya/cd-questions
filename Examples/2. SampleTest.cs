f591074b9221c8f4dcfc697ae7a3657982b57f86
yarpskendaya <jangohemmes@hotmail.com>
2023-11-21 18:45:55 +0100

    Flesh out examples

--- Examples/2. SampleTest.cs
+++ Examples/2. SampleTest.cs
@@ -9,9 +9,45 @@ public class GridCellConfiguratorTests
         Vector3 cellPosition = new(-455, 77.7, 0);
         IModelConfigurator modelConfigurator = CreateModelConfigurator();
         Transform expected = new(new Vector3(45, 67.7, 90), Rotation.Zero, false);
-
-        CreateConfigurator(modelConfigurator).Configure(_stubDoc, grid, Create.Transform(cellPosition));
+        GridCellConfigurator gridConfigurator = CreateConfigurator(modelConfigurator);
+        
+        gridConfigurator.Configure(_stubDoc, grid, Create.Transform(cellPosition));
 
         modelConfigurator.Received().Configure(_stubDoc, model.Model, expected);
     }
+
+    private IModelConfigurator CreateModelConfigurator()
+    {
+        // return stub;
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+public static class Create {}
+public class GridCellConfigurator
+{
+    public void Configure(IDocument doc, HouseModelAssembly model, Transform transform)
+    {
+    }
+}
+public class GridCellAssembly {}
+public class HouseModelAssembly {}
+public class Vector3 {}
+public class Rotation {}
+public class Transform {}
+public interface IModelConfigurator {}
--- Examples/3. TestScope.cs
+++ Examples/3. TestScope.cs
@@ -1,15 +1,35 @@
-public class GridCellConfiguratorTests : HelperScope
+public class CalculatorTests
 {
+    private TestScope _scope = new();
+
     [Test]
-    public void ShouldNotThrowWhenGivenNoCell()
+    public void AddsTwoNumbers()
     {
-        //TODO: Flesh this out
-        GivenAGridWithoutAnyCells();
+        _scope.WhenAddingTwoNumbers(1, 2);
+        _scope.TheResultShouldBe(3);
     }
 }
 
-
-public class HelperScope
+public class TestScope
 {
+    private Calculator _calculator = new();
+    private int sum;
 
+    public void WhenAddingTwoNumbers(int a, int b)
+    {
+        sum = _calculator.Add(a, b);
+    }
+
+    public void TheResultShouldBe(int expected)
+    {
+        Assert.AreEqual(expected, sum);
+    }
+}
+
+public class Calculator
+{
+    public int Add(int a, int b)
+    {
+        return a + b;
+    }
 }
